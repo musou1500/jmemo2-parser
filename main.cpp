e0b17280eba8dfdf24235abd638628d15f001f5b
musou1500 <musou1500@gmail.com>
2016-07-15 23:38:49 +0900

    MakefileからCMakeへ移行した

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ using namespace std;
 
 int main(int argc, char const* argv[])
 {
+	string filename;
+	cout << "Jmemo2-Parser CLI" << endl;
+	cout << "memo file ->" << endl;
+	cin >> filename;
     Jmemo2Parser parser;
-    parser.initWithFileName("music/SABM.txt");
+    parser.initWithFileName(filename);
     for(auto note : parser.getNotes()) {
         cout << note.panelIndex << ",";
         cout << note.justTime << endl;
